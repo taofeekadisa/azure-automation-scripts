34402f60bbff51a64ac49adf49765c3dff87f310
taofeekadisa <adisataofeekabiodun@gmail.com>
2023-04-17 23:13:42 +0300

    Bash script to create resource group

---
+++ create_nsg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#CREATE NETWORK SECURITY GROUP
+#AUTHOR : TAOFEEK ADISA
+
+#CREATE A VARIABLE TO STORE THE LIST OF NSGs
+readarray -t nsg_list < <(echo $(az network nsg list --query "[].name"))
+
+#CREATE A VARIABLE FOR THE NSG NAME, RESOURCE GROUP, REGION
+nsg_name=dev-nsg
+resource_group=hator-dev-rg
+region=eastus2
+
+#CHECK IF THE NSG EXIST BEFORE CREATING
+
+if [[ "$nsg_list" =~ "$nsg_name" ]]
+then
+	echo "Network Security Group Exist"
+else
+	echo $(az network nsg create -n $nsg_name -g $resource_group -l $region)
+fi
